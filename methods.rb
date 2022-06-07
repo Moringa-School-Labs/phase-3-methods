1d45bceb368ef9de9a0e3a81d1b8db9be47474b7
catherine <vuthi.esther@gmail.com>
2022-06-07 09:02:21 +0300

    commit tree 0cc956ac61ac1efa6c0cdf6a7810304fe8a8ed5e

--- methods.rb
+++ methods.rb
@@ -1,1 +1,34 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+end
+greet "Jimmy"
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+greet_with_default "Cate"
+greet_with_default
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+add(2,6)
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+
+    return num/2
+end
+
+halve 8
